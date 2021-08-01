822f740141e475a3b588739011a71b82ccda8513
ouyangyuro <def2446@gmail.com>
2021-08-01 09:15:46 +0800

    add sum other way

--- basic/sum.js
+++ basic/sum.js
@@ -1,13 +1,20 @@
 console.log("hello");
 
-function sum(n){
-    let result = 0;
-    for(let i = 0; i <= n ; i++){
-        result += i; 
-    }
+// way 1
+// function sum(n) {
+//     let result = 0;
+//     for (let i = 1; i <= n; i++) {
+//         result += i;
+//     }
+//     return result;
+// }
+
+// way 2 better
+function sum(n) {
+    let result = ((n+1)*n)/2;
     return result;
 }
 
-console.log(sum(2));
-console.log(sum(3));
-console.log(sum(10));
+// console.log(sum(1));
+// console.log(sum(3));
+// console.log(sum(10));

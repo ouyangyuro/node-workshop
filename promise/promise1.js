6485f1863367487a0e2b1513162a26e43a9664d6
ouyangyuro <def2446@gmail.com>
2021-08-06 14:12:15 +0800

    改良 promise1

--- promise/promise1.js
+++ promise/promise1.js
@@ -16,11 +16,34 @@ let dt = new Date();
 console.log(`開始工作 at ${dt.toISOString()}`);
 
 
-let job1 = doWork("刷牙", 3000, false);
+let job1 = doWork("刷牙", 3000, true);
 // 刷牙 -> 吃早餐 -> 寫功課
 job1.then(
     function(resolve){
         console.log("first function:",resolve);
+
+        let job2 = doWork("吃早餐", 5000, true);
+        job2.then(
+            function(resolve){
+                console.log("first function:",resolve);
+
+                let job3 = doWork("寫功課", 3000, true);
+                job3.then(
+                    function(resolve){
+                        console.log("first function:",resolve);
+                        
+                    },
+                    function(reject){
+                        console.log("second function:",reject);
+                    }
+                );
+                
+            },
+            function(reject){
+                console.log("second function:",reject);
+            }
+        );
+
     },
     function(reject){
         console.log("second function:",reject);

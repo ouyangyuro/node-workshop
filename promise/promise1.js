ad729660cfa1fbe1f22f900cbcadfc623254a1cd
ouyangyuro <def2446@gmail.com>
2021-08-06 15:33:26 +0800

    then版本 promise1

--- promise/promise1.js
+++ promise/promise1.js
@@ -18,35 +18,51 @@ console.log(`開始工作 at ${dt.toISOString()}`);
 
 let job1 = doWork("刷牙", 3000, true);
 // 刷牙 -> 吃早餐 -> 寫功課
-job1.then(
-    function(resolve){
-        console.log("first function:",resolve);
+job1
+    .then((resolve)=>{
+        console.log("first",resolve)
+        return doWork("吃早餐", 5000, true);
+    })
+    .then((resolve)=>{
+        console.log("second",resolve)
+        return doWork("寫功課", 3000, true);
+    })
+    .then((resolve)=>{
+        console.log("third",resolve);
+    })
 
-        let job2 = doWork("吃早餐", 5000, true);
-        job2.then(
-            function(resolve){
-                console.log("first function:",resolve);
+// console.log(job1);//pennding
+// job1.then(
+//     function(resolve){
+//         console.log("first function:",resolve);
+//         console.log(job1);
 
-                let job3 = doWork("寫功課", 3000, true);
-                job3.then(
-                    function(resolve){
-                        console.log("first function:",resolve);
+//         let job2 = doWork("吃早餐", 5000, true);
+//         job2.then(
+//             function(resolve){
+//                 console.log("first function:",resolve);
+
+//                 let job3 = doWork("寫功課", 3000, true);
+//                 job3.then(
+//                     function(resolve){
+//                         console.log("first function:",resolve);
                         
-                    },
-                    function(reject){
-                        console.log("second function:",reject);
-                    }
-                );
+//                     },
+//                     function(reject){
+//                         console.log("second function:",reject);
+//                     }
+//                 );
                 
-            },
-            function(reject){
-                console.log("second function:",reject);
-            }
-        );
+//             },
+//             function(reject){
+//                 console.log("second function:",reject);
+//             }
+//         );
 
-    },
-    function(reject){
-        console.log("second function:",reject);
-    }
-);
+//     },
+//     function(reject){
+//         console.log("second function:",reject);
+//         console.log(job1); //rejected
+//     }
+// );
 

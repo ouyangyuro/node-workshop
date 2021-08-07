45f98972a8accabaf753fcaa320a359cbdcbf603
ouyangyuro <def2446@gmail.com>
2021-08-07 19:33:50 +0800

    8/7 作業一callback版本

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,14 +1,23 @@
 const axios = require("axios");
 const moment = require("moment");
 // console.log(moment().format("YYYYMMDD"));
+const fs = require("fs");
 
-axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY",{
-    params:{
-        response: "json",
-        date:moment().format("YYYYMMDD"),
-        stockNo:"2330",
-    },
+fs.readFile("crawler/stock.txt", "utf8", (err, data) => {
+    // console.log(data);
+    if(err){
+        console.error(err);
+    }
+    else{
+        axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+            params: {
+                response: "json",
+                date: moment().format("YYYYMMDD"),
+                stockNo: data,
+            },
+        })
+            .then((response) => {
+                console.log(response.data);
+            });
+    }
 })
-.then((response)=>{
-    console.log(response.data);
-});

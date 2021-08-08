0a5b92219bd39e5f7af526e6f87aa478b3a4b991
ouyangyuro <def2446@gmail.com>
2021-08-08 10:38:25 +0800

    8/7 挑戰題改善

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -5,40 +5,38 @@ const fs = require("fs");
 
 async function getData() {
 
-    try {
+    let getStockNo = await new Promise((res, rej) => { //等Promise物件read file後在執行下面的axios
 
-        let getStockNo = await new Promise((res, rej) => { //等Promise物件read file後在執行下面的axios
-
-            fs.readFile("crawler/stock.txt", "utf8", (err, data) => { //非同步 //把判斷對錯放Promise物件裡
-                // console.log(data);
-                if (err) {
-                    // rej(console.log("rej", err)); //for check
-                    rej(err);
-                }
-                else {
-                    // res(console.log("res", data)); //for check
-                    res(data);
-                }
-            })
-
-        });
-        // console.log(getStockNo); //for check
-
-
-        axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", { //等上面await執行完才會執行
-            params: {
-                response: "json",
-                date: moment().format("YYYYMMDD"),
-                stockNo: getStockNo,
-            },
+        fs.readFile("crawler/stock.txt", "utf8", (err, data) => { //非同步 //把判斷對錯放Promise物件裡
+            // console.log(data);
+            if (err) {
+                // rej(console.log("rej", err)); //for check
+                rej(err);
+            }
+            else {
+                // res(console.log("res", data)); //for check
+                res(data);
+            }
         })
-            .then((response) => {
-                console.log(response.data);
-            });
 
-    }
-    catch (err) {
-        console.error(err);
-    }
+    });
+    // console.log(getStockNo); //for check
+
+
+    let getData = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", { //等上面await執行完才會執行
+        params: {
+            response: "json",
+            date: moment().format("YYYYMMDD"),
+            stockNo: getStockNo,
+        },
+    })
+    console.log(getData.data.title);
+
 }
-getData();
+
+try {
+    getData();
+}
+catch (err) {
+    console.error(err);
+}

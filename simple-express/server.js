9ff9c589945f8580f937dbe9ef81e10a06324d82
ouyangyuro <def2446@gmail.com>
2021-08-15 11:33:28 +0800

    8/15 開發兩個middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,15 +3,28 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+//使用一個"中間件"(middleware)
+app.use((req, res, next) => {
+  let current = new Date();
+  console.log(`有人來訪問了 at ${current.toISOString()}`);
+  console.log("i am first middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("i am second middleware");
+  next();
+});
+
 //HTTP Method: get, post, put, patch, delete
-app.get("/", function(requset, response, next){
-    response.send("Hello Home")
-})
+app.get("/", function (requset, response, next) {
+  response.send("Hello Home");
+});
 
-app.get("/about", function(requset, response, next){
-    response.send("about")
-})
+app.get("/about", function (requset, response, next) {
+  response.send("Hello about");
+});
 
-app.listen(3000, ()=>{
-    console.log("star web server");
-})
+app.listen(3000, () => {
+  console.log("star web server");
+});
